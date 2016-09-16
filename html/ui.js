95f96d58d9d5fbcf547df124f32e7dd502c236ef
beegee-tokyo <bernd@giesecke.tk>
2016-09-16 22:53:58 +0800

    Added my personal flavor to the ESP8266 configuration home page
    Signed-off-by: beegee-tokyo <bernd@giesecke.tk>

--- html/ui.js
+++ html/ui.js
@@ -232,7 +232,7 @@ onLoad(function() {
   var mm = m(
    '<div id="menu">\
       <div class="pure-menu">\
-        <a class="pure-menu-heading" href="https://github.com/jeelabs/esp-link">\
+        <a class="pure-menu-heading" href="https://github.com/beegee-tokyo/esp-link/tree/beegee-flavor">\
         <img src="/favicon.ico" height="32">&nbsp;esp-link</a>\
         <div class="pure-menu-heading system-name" style="padding: 0px 0.6em"></div>\
         <ul id="menu-list" class="pure-menu-list"></ul>\
